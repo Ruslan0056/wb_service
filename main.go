91e5597f14b4bd378e8c0e48f25da7bf318962a3
Ruslan0056 <rus94.5610@gmail.com>
2023-04-19 17:16:21 +0300

    vers_0.1

--- main.go
+++ main.go
@@ -53,14 +53,47 @@ func main() {
 	weekly_buyout := target_revenue / 4 * 100
 	buyout_amt := weekly_buyout / product_price //количество выкупов на неделю
 	var amt_product_by_day []float64
+	option := 0
+	fmt.Println("Выберите сценарий:")
+	fmt.Println("1 - Низходящая регрессия;")
+	fmt.Println("2 - Возрастающая регрессия;")
+	fmt.Println("3 - Волнобразный сценарий;")
+	fmt.Println("4 - Сценарий с пиками по краям.")
+	fmt.Scanln(&option)
+
 	sum_result := 0.0
-	for i := 1; i < 8; i++ {
-		result := 0.2208 * (math.Pow(float64(i), -0.384))
-		amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
-		sum_result += result * float64(buyout_amt) / 100
+	switch option {
 
+	case 1:
+
+		for i := 1; i < 8; i++ {
+			result := 0.2208 * (math.Pow(float64(i), -0.384))
+			amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
+			sum_result += result * float64(buyout_amt) / 100
+
+		}
+
+	case 2:
+		for i := 1; i < 8; i++ {
+			result := 4.2738 * math.Exp(0.1021*float64(i))
+			amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
+			sum_result += result * float64(buyout_amt) / 100
+		}
+
+	case 3:
+		for i := 1; i < 8; i++ {
+			result := -0.1111*math.Pow(float64(i), 3) + 1.3333*math.Pow(float64(i), 2) - 4.6984*float64(i) + 8.5714 // низходящий
+			//result2 := 0.0333 * math.Pow(i,5) - 0.6667 * math.Pow(i,4) + 4.8333 * math.Pow(i,3) - 15.333 * math.Pow(i,2) - 20.133 * i + 4
+			amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
+			sum_result += result * float64(buyout_amt) / 100
+		}
+	case 4:
+		for i := 1; i < 8; i++ {
+			result := 0.5238*math.Pow(float64(i), 2) - 4.1905*float64(i) + 13.857
+			amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
+			sum_result += result * float64(buyout_amt) / 100
+		}
+		fmt.Printf("%.3f\n",amt_product_by_day)
+		fmt.Println(int(sum_result))
 	}
-
-	fmt.Println(amt_product_by_day)
-	fmt.Println(int(sum_result))
 }
