b3a33f0889108252cb93cb6c2a8904462f46789a
Ruslan0056 <rus94.5610@gmail.com>
2023-04-20 22:30:20 +0300

    v_0.2

--- main.go
+++ main.go
@@ -22,8 +22,13 @@ import (
 	"math"
 )
 
-type wb_service struct {
-}
+// type wb_service struct {
+// 	product_price int
+// 	target_revenue int
+// 	amt_keywords int
+// 	keywords []string
+// 	proportion []float64
+// }
 
 func main() {
 
@@ -36,7 +41,7 @@ func main() {
 	fmt.Scanln(&target_revenue)
 	fmt.Print("Количество ключевых слов: ")
 	fmt.Scanln(&amt_keywords)
-
+	
 	for i := 0; i < amt_keywords; i++ {
 		var a string
 		var b float64
@@ -47,53 +52,81 @@ func main() {
 		keywords = append(keywords, a)
 		proportion = append(proportion, b)
 	}
-
+	
 	//fmt.Println(product_price, target_revenue, amt_keywords, keywords)
-
+	
 	weekly_buyout := target_revenue / 4 * 100
 	buyout_amt := weekly_buyout / product_price //количество выкупов на неделю
-	var amt_product_by_day []float64
-	option := 0
-	fmt.Println("Выберите сценарий:")
-	fmt.Println("1 - Низходящая регрессия;")
-	fmt.Println("2 - Возрастающая регрессия;")
-	fmt.Println("3 - Волнобразный сценарий;")
-	fmt.Println("4 - Сценарий с пиками по краям.")
-	fmt.Scanln(&option)
-
+	var amt_product_by_day [][]int            //выделить память
+	// option := 0
+	// fmt.Println("Выберите сценарий:")
+	// fmt.Println("1 - Низходящая регрессия;")
+	// fmt.Println("2 - Возрастающая регрессия;")
+	// fmt.Println("3 - Волнобразный сценарий;")
+	// fmt.Println("4 - Сценарий с пиками по краям.")
+	// fmt.Scanln(&option)
+	
 	sum_result := 0.0
-	switch option {
-
-	case 1:
-
-		for i := 1; i < 8; i++ {
-			result := 0.2208 * (math.Pow(float64(i), -0.384))
-			amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
-			sum_result += result * float64(buyout_amt) / 100
-
+	
+	
+	for i := 1; i < 8; i++ {
+		result := 0.5238*math.Pow(float64(i), 2) - 4.1905*float64(i) + 13.857
+		sum_result += result * float64(buyout_amt) / 100
+		var amt_things_by_keyword []int 
+		for j := 0; j < amt_keywords; j++ {
+			amt_things_by_keyword = append(amt_things_by_keyword, int(result*float64(buyout_amt)/100 * proportion[j]))
 		}
-
-	case 2:
-		for i := 1; i < 8; i++ {
-			result := 4.2738 * math.Exp(0.1021*float64(i))
-			amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
-			sum_result += result * float64(buyout_amt) / 100
-		}
-
-	case 3:
-		for i := 1; i < 8; i++ {
-			result := -0.1111*math.Pow(float64(i), 3) + 1.3333*math.Pow(float64(i), 2) - 4.6984*float64(i) + 8.5714 // низходящий
-			//result2 := 0.0333 * math.Pow(i,5) - 0.6667 * math.Pow(i,4) + 4.8333 * math.Pow(i,3) - 15.333 * math.Pow(i,2) - 20.133 * i + 4
-			amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
-			sum_result += result * float64(buyout_amt) / 100
-		}
-	case 4:
-		for i := 1; i < 8; i++ {
-			result := 0.5238*math.Pow(float64(i), 2) - 4.1905*float64(i) + 13.857
-			amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
-			sum_result += result * float64(buyout_amt) / 100
-		}
-		fmt.Printf("%.3f\n",amt_product_by_day)
-		fmt.Println(int(sum_result))
+		amt_product_by_day = append(amt_product_by_day, amt_things_by_keyword)
 	}
+	// switch option {
+		
+	// case 1:
+
+	// 	for i := 1; i < 8; i++ {
+	// 		result := 0.2208 * (math.Pow(float64(i), -0.384))
+	// 		amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
+	// 		sum_result += result * float64(buyout_amt) / 100
+
+	// 	}
+
+	// case 2:
+	// 	for i := 1; i < 8; i++ {
+	// 		result := 4.2738 * math.Exp(0.1021*float64(i))
+	// 		amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
+	// 		sum_result += result * float64(buyout_amt) / 100
+	// 	}
+
+	// case 3:
+	// 	for i := 1; i < 8; i++ {
+	// 		result := -0.1111*math.Pow(float64(i), 3) + 1.3333*math.Pow(float64(i), 2) - 4.6984*float64(i) + 8.5714 // низходящий
+	// 		//result2 := 0.0333 * math.Pow(i,5) - 0.6667 * math.Pow(i,4) + 4.8333 * math.Pow(i,3) - 15.333 * math.Pow(i,2) - 20.133 * i + 4
+	// 		amt_product_by_day = append(amt_product_by_day, result*float64(buyout_amt)/100)
+	// 		sum_result += result * float64(buyout_amt) / 100
+	// 	}
+	// case 4:
+	// }
+	
+
+	fmt.Println(amt_product_by_day)
+
+	fmt.Println(int(sum_result))
+
+
+
+	//размерная сетка
+	// size_chart := make(map[string]float64)
+	// amt_size := 5
+
+	// for i := 0; i < amt_size; i++ {
+	// 	fmt.Println("Введите размер")
+	// 	size := ""
+	// 	fmt.Scanln(&size)
+	// 	fmt.Println("Ввведите долю выкупа")
+	// 	proportion_size := 0.0
+	// 	fmt.Scanln(&proportion_size)
+	// 	size_chart[size] = proportion_size
+	// }
+
+	// fmt.Println(size_chart)
+
 }
